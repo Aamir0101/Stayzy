62b89d87f6cb990a6c2923c84811e493c26ba769
aamir <mohdaamir726267@gmail.com>
2026-02-06 16:03:52 +0530

    initialized database

--- init/index.js
+++ init/index.js
@@ -14,6 +14,14 @@ async function main() {
   await mongoose.connect(dbUrl);
 }
 
+const store= new MongoStore({
+  mongoUrl: dbUrl,
+  crypto:{
+    secret: process.env.SECRET
+  },
+  touchAfter: 24 * 3600,
+});
+
 const initDB= async ()=>{
     await Listing.deleteMany({});
     initData.data= initData.data.map((obj)=>({...obj, owner: "695aa6a069279129b76f8c02"}))
