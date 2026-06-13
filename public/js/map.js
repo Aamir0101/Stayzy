958d1b5263a7844e68230dde6defb4f3692de603
Aamir0101 <mohdaamir88765@gmail.com>
2026-06-13 19:26:38 +0530

    Refactor marker creation and coordinate usage

--- public/js/map.js
+++ public/js/map.js
@@ -1,11 +1,12 @@
+const coordinates = listing.geometry.coordinates;
+
 const map = new maplibregl.Map({
   container: 'map',
-  style: `https://maps.geoapify.com/v1/styles/klokantech-basic/style.json?apiKey=${mapToken}`, // stylesheet location
-  center: coordinates, // starting position [lng, lat]
-  zoom: 9 // starting zoom
+  style: `https://maps.geoapify.com/v1/styles/klokantech-basic/style.json?apiKey=${mapToken}`,
+  center: coordinates,
+  zoom: 9
 });
 
-
-const marker= new maplibregl.Marker({ color: "red" })
-    .setLngLat(listing.geometry.coordinates)
-    .addTo(map);
+new maplibregl.Marker({ color: "red" })
+  .setLngLat(coordinates)
+  .addTo(map);
